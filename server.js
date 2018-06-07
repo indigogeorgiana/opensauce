4cc4b82d20348b16eb7d601bdf91d0a1bd1cbcd5
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-07 19:09:37 +1200

    added basic server setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const path = require('path')
+const routes = require('./routes')
+
+const server = express()
+
+const hbsConfig = {
+  defaultLayout: 'main',
+  extname: 'hbs'
+}
+server.engine('hbs', hbs(hbsConfig))
+server.set('view engine', 'hbs')
+
+server.use(express.urlencoded({ extended: false }))
+server.use(express.static(path.join(__dirname, 'public')))
+server.use('/', routes)
