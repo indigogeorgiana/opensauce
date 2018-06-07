3a185dad6566a9be7429ce89ce47d552572704cf
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-07 20:41:22 +1200

    changes

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('./layouts/main')
+  res.render('./templates/index')
 })
+
+router.get('/cohort', (req, res) => {
+  res.render('./templates/cohort')
+})
+router.get('/cohort', (req, res) => {
+  res.render('./templates/cohort')
+})
+
+// router.get('/')
 
 module.exports = router
