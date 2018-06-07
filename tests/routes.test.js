9c289a9684fd5698fb106d18263a8b93af79597c
Caitlyn <caitlyn.bayley@gmail.com>
2018-06-08 11:43:26 +1200

    test 2 fail

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -3,13 +3,40 @@ const cheerio = require('cheerio')
 const routes = require('../routes/routes')
 const server = require('../server')
 
-test('GET root route page', done => {
+test('all systems go', (done) => {
+  expect(true).toBeTruthy()
+  done()
+})
+
+test('test route to index', done => {
   request(server)
     .get('/')
     .end((err, res) => {
       const $ = cheerio.load(res.text)
-      const title = $('title').text()
-      expect(title).toMatch('Open Sauce')
+      const label = $('label[for=name]').text()
+      expect(label).toMatch('Name')
       done(err)
     })
 })
+
+test('test route / to /cohort', done => {
+  request(server)
+    .get('/cohort')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const header = $('')
+      expect().toMatch('')
+      done(err)
+    })
+})
+
+// test('test route /cohort to /profile', done => {
+//   request(server)
+//     .get('/profile')
+//     .end((err, res) => {
+//       const $ = cheerio.load(res.text)
+//       const p = $('p').text()
+//       expect(p).toMatch('Name')
+//       done(err)
+//     })
+// })
