5ed0059d592772d8753601f7548a38085d84e746
Indigo Phillips <indigogeorgiana@gmail.com>
2018-06-07 20:32:55 +1200

    Altered css folder

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,14 +2,58 @@ const request = require('supertest')
 const cheerio = require('cheerio')
 const server = require('../server')
 
-test('test route to homepage', done => {
-// Arrange
-// Act
+test('GET root route page', done => {
   request(server)
     .get('/')
     .end((err, res) => {
-// Assert
-      expect(err).toBeFalsy()
-      expect(res.text).toBe(expected)
+      const $ = cheerio.load(res.text)
+      const h1 = $('').text()
+      expect(h1).toMatch('')
+      done(err)
+    })
+})
+
+test('GET profiles page', done => {
+  request(server)
+    .get(' ')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const h1 = $(' ').text()
+      expect(h1).toMatch(' ')
+      done(err)
+    })
+})
+
+test('GET cohort page', done => {
+  request(server)
+    .get(' ')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const h1 = $(' ').text()
+      expect(h1).toMatch(' ')
+      done(err)
+    })
+})
+
+test('GET edit profile page form', done => {
+  request(server)
+    .get(' ')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text)
+      const h1 = $(' ').attr(' ')
+      console.log(h1)
+      expect(h1).toMatch(' ')
+      done(err)
+    })
+})
+
+test('POST to data json and redirect', done => {
+  request(server)
+    .post(' ')
+    .send(' ')
+    .end((err, res) => {
+      expect(res.statusCode).toBe(302)
+      expect(res.text).toBe('v ')
+      done(err)
     })
 })
