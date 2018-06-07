6f67de13a91e0084637ef13ca81a0b6aedbd0f87
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-08 07:50:37 +1200

    update

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const getData = require('../getData.js')
 
 router.get('/', (req, res) => {
   res.render('./templates/index')
@@ -12,6 +13,18 @@ router.get('/cohort', (req, res) => {
   res.render('./templates/cohort')
 })
 
+router.get('/puppies/:id', (req, res) => {
+  const id = Number(req.params.id)
+  getData((err, data) => {
+    if (err) {
+      res.send('unable to read data file').status(500)
+    } else {
+      const peeps = JSON.parse(data)
+      const person = peeps.peeps.find(peep => peep.id === id)
+      res.render('./templates/profile', person)
+    }
+  })
+})
 // router.get('/')
 
 module.exports = router
