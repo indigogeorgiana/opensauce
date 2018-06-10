ca65b3792f053c05b7397413be036777770a380a
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-10 23:07:48 +1200

    sign in page now requires username and password, you all know what it is :D

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const getData = require('../getData.js')
-const addData = require('../addData.js')
 
 router.get('/', (req, res) => {
   res.render('./templates/index')
