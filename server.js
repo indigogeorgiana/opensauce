e579f53d001ef46bee739f296bf102d422f20697
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-06-10 13:49:48 +1200

    refactored routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const path = require('path')
 const routes = require('./routes/routes')
+const mamaRoutes = require('./routes/mamaRoutes')
 
 const server = express()
 
@@ -16,5 +17,6 @@ server.set('view engine', 'hbs')
 server.use(express.urlencoded({ extended: false }))
 server.use(express.static(path.join(__dirname, 'public')))
 server.use('/', routes)
+server.use('/mamaku', mamaRoutes)
 
 module.exports = server
